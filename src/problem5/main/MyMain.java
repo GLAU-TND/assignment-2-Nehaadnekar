3b4242903a573b41d0aec08d8b61d5682742b90a
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2020-03-27 10:58:09 -0700

    Update MyMain.java

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -7,22 +7,19 @@
 package problem5.main;
 
 import problem5.circularqueue.MyCircularQueue;
-import problem5.student.Student;
 
 //executable class
 public class MyMain {
     public static void main(String[] args) {
-        MyCircularQueue myCircularQueue  =new MyCircularQueue(3);
-        myCircularQueue.enqueue(new Student("T",0,0));
-        myCircularQueue.enqueue(new Student("Y",1,1));
-        myCircularQueue.enqueue(new Student("D",1,0));
-        for(int i=0;i<myCircularQueue.getSize()+1;i++){
-            Student s = myCircularQueue.dequeue();
-//            System.out.println(s);
-            if(s.getBackLog()-s.getAppearingCount()!=0){
-                myCircularQueue.enqueue(s);
-            }
-        }
-        myCircularQueue.show();
+        MyCircularQueue obj = new MyCircularQueue();
+        obj.enqueue(1,4,0);
+        obj.enqueue(2,4,1);
+        obj.enqueue(3,4,4);
+        obj.enqueue(4,2,1);
+        obj.enqueue(5,2,2);
+        obj.enqueue(6,0,0);
+        obj.removeBackLog();
+        obj.display();
+
     }
 }
