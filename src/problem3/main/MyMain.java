1868674c8adbaabe013aad43047e408fe5f50cad
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2020-03-27 09:59:31 -0700

    Update MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,24 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+import problem3.myqueue.MyPriorityQueue;
+import problem3.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.enqueue(new Student("Neha",23));
+        queue.enqueue(new Student("Mani",17));
+        queue.enqueue(new Student("Prachi",26));
+        queue.enqueue(new Student("Reema",35));
+        queue.enqueue(new Student("Priyanka",33));
+        queue.enqueue(new Student("Nishtha",24));
+
+        queue.show();
+        queue.dequeue();
+        queue.show();
+
+    }
 }
